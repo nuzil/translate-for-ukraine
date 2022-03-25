d3216bdc08506e3fbc2c4e63e2de569b86515bf3
Lyzun Oleksandr <alex.nuzil@gmail.com>
2022-03-25 17:16:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Translate for Ukraine</title>
-    <link rel="icon" href="lib/images/favicon.svg" type="image/svg+xml">
-    <script src="jquery-3.5.1.min.js"></script>
-    <link rel="stylesheet" href="lib/css/styles.min.css">
-</head>
-<body>
-    <?php include("header.php");>
+<?php include("header.php");>
     <section>
         <article>
             <header>
@@ -112,6 +100,3 @@
             }
         });
 </script>
-</body>
-</html>
-
