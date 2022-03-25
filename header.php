ca6df1b0e83351c7388f21d8724f3616107c6463
Lyzun Oleksandr <alex.nuzil@gmail.com>
2022-03-25 17:10:38 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <!-- <a href="/">Home</a> -->
+                </li>
+                <li>
+                    <a class="logo" href="/">
+                        <img src="lib/images/logo.svg" alt="Translate for Ukraine">
+                    </a>
+                </li>
+                <li>
+                    <!-- <a href="/faq">FAQ</a> -->
+                </li>
+            </ul>
+        </nav>
+    </header>
