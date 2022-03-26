f728eddd09d3be01d07d68b5e8acac4c9a54ddf0
ivanko <i.koliadynskyy@gmail.com>
2022-03-26 12:49:41 +0100

    Footer updates

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
     <footer>
-        <h3 class="footer-tittle">More information: </h3>
         <div class="footer-container">
-            <ul class="footer-menu">
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-            </ul>
-            <ul class="footer-menu">
-                <li><a href="#">Link 1</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-            </ul>
+            <h3 class="footer-tittle">More information: </h3>
+            <div class="footer-menu-container">
+                <ul class="footer-menu">
+                    <li>
+                        <a href="https://docs.google.com/forms/d/1L4wWTgjNXY7CgoYM3PkHAy3rOn1mKXrAcGrVwI4G3ME">
+                            Registrieren Sie sich als Freiwilliger
+                        </a>
+                    </li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                </ul>
+                <ul class="footer-menu">
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                </ul>
+            </div>
         </div>
     </footer>
 </body>
