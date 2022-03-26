88056b984a81d5abca25968e3f9b3022d2b6d6c9
ivanko <i.koliadynskyy@gmail.com>
2022-03-26 09:01:52 +0100

    Dummy footer + minor fixes

--- footer.php
+++ footer.php
@@ -1,5 +1,17 @@
     <footer>
-	</footer>
-
-  </body>
-  </html>
+        <h3 class="footer-tittle">More information: </h3>
+        <div class="footer-container">
+            <ul class="footer-menu">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+            <ul class="footer-menu">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
