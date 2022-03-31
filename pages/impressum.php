43b9c25cdaaf37bf2b2e614c8c12ca49d0cdc193
olexiyzalevskyi <82677692+olexiyzalevskyi@users.noreply.github.com>
2022-03-31 23:10:12 +0200

    Update impressum.php

--- pages/impressum.php
+++ pages/impressum.php
@@ -1,7 +1,175 @@
 <?php include("../header.php"); ?>
 <section>
     <article>
-        Impressum
+        <h1>Impressum</h1>
+<h5>INTAMT | INTERNATIONALE AKADEMIE F&Uuml;R MANAGEMENT UND TECHNOLOGIE e. V.</h5>
+<p>Alt-Pempelfort 15<br />40211 D&uuml;sseldorf&nbsp;</p>
+<p>Tel. + 49 (0)211 5504450<br />Fax + 49 (0)211 5504454</p>
+<p>Web&nbsp;<a href="http://www.intamt.de/" target="_blank" rel="noopener">www.intamt.de</a><br />Mail&nbsp;<a href="mailto:intamt@intamt.de">intamt(at)intamt.de</a></p>
+<p><strong>Vertretungsberechtigter Vorstand<br /></strong>Yuri Nikitin (<span class="internal-link">Gesch&auml;ftsf&uuml;hrer)</span>, Prof. Dr. Eduard Patrik (Vorsitzender), Dr. Vladimir Fridland&nbsp;</p>
+<p><strong>Registergericht<br /></strong>Amtsgericht D&uuml;sseldorf</p>
+<p><strong>Registernummer<br /></strong>VR 9527</p>
+<p><strong>Inhaltlich Verantwortlicher gem&auml;&szlig; &sect; 6 MDStV<br /></strong>Gem. &sect; 55 Abs. 2 RStV: Der Vorstand des Vereins (siehe &ldquo;Vertretungsberechtigte Personen&rdquo;)</p>
+<h2>Haftungsausschluss</h2>
+<p>Wir weisen hiermit darauf hin, dass die Nutzung jeglicher Art von Informationen, die wir auf den Internetseiten der Akademie zur Verf&uuml;gung stellen, auf eigene Gefahr erfolgt. Wir behalten uns ausdr&uuml;cklich vor, den Inhalt dieser Internetseiten jederzeit zu &auml;ndern. Anspr&uuml;che jeglicher Art, die sich aus dem Inhalt und den &Auml;nderungen ergeben k&ouml;nnten, sind ausgeschlossen.</p>
+<p>Eine Garantie oder Haftung, insbesondere f&uuml;r direkte oder indirekte Sch&auml;den, die durch die Nutzung der Informationen auf den Seiten dieser Website entstehen, wird nicht &uuml;bernommen.</p>
+<p>Mit Urteil vom 12.5.1998 (-312 O 85/98 &ndash; &bdquo;Haftung f&uuml;r Links&ldquo;) hat das Landgericht Hamburg entschieden, dass man durch die Anbringung eines Links die Inhalte der gelinkten Seite gegebenenfalls mit zu verantworten hat. Dies kann nur dadurch verhindert werden, dass man sich ausdr&uuml;cklich von diesen Inhalten distanziert.</p>
+<p>Auf dieser Website sind Links zu anderen Inhalten im Internet vorhanden. Wir weisen ausdr&uuml;cklich darauf hin, dass wir keinerlei Einfluss auf die Gestaltung und die Inhalte der gelinkten Seiten/Inhalte haben. Deshalb distanzieren wir uns von allen Inhalten aller gelinkten Seiten auf dieser Website.</p>
+<h2>Datenschutzerkl&auml;rung</h2>
+<p><strong>Allgemeiner Hinweis und Pflichtinformationen</strong></p>
+<p><strong>Benennung der verantwortlichen Stelle</strong></p>
+<p>Die verantwortliche Stelle f&uuml;r die Datenverarbeitung auf dieser Website ist:</p>
+<p>INTAMT&nbsp;&nbsp;e.V.<br />Alt Pempelfort 15<br />40211&nbsp;D&uuml;sseldorf</p>
+<p>Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen &uuml;ber die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. &Auml;.).</p>
+<p><strong>Widerruf Ihrer Einwilligung zur Datenverarbeitung</strong></p>
+<p>Nur mit Ihrer ausdr&uuml;cklichen Einwilligung sind einige Vorg&auml;nge der Datenverarbeitung m&ouml;glich. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit m&ouml;glich. F&uuml;r den Widerruf gen&uuml;gt eine formlose Mitteilung per E-Mail. Die Rechtm&auml;&szlig;igkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unber&uuml;hrt.</p>
+<h2>Widerspruchs- und Beseitigungsm&ouml;glichkeit</h2>
+<p>Die Erfassung der Daten zur Bereitstellung der Website und die Speicherung der Daten in Logfiles ist f&uuml;r den Betrieb der Internetseite zwingend erforderlich. Es besteht folglich seitens des Nutzers keine Widerspruchsm&ouml;glichkeit. Mit der Nutzung der Webseite stimmt der Nutzer dem zu.</p>
+<p><strong>Recht auf Beschwerde bei der zust&auml;ndigen Aufsichtsbeh&ouml;rde</strong></p>
+<p>Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Versto&szlig;es ein Beschwerderecht bei der zust&auml;ndigen Aufsichtsbeh&ouml;rde zu. Zust&auml;ndige Aufsichtsbeh&ouml;rde bez&uuml;glich datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem sich der Sitz unseres Unternehmens befindet. Der folgende Link stellt eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten bereit:&nbsp;<a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" target="_blank" rel="noopener">https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html</a>.</p>
+<p><strong>Recht auf Daten&uuml;bertragbarkeit</strong></p>
+<p>Ihnen steht das Recht zu, Daten, die wir auf Grundlage Ihrer Einwilligung oder in Erf&uuml;llung eines Vertrags automatisiert verarbeiten, an sich oder an Dritte aush&auml;ndigen zu lassen. Die Bereitstellung erfolgt in einem maschinenlesbaren Format. Sofern Sie die direkte &Uuml;bertragung der Daten an einen anderen Verantwortlichen verlangen, erfolgt dies nur, soweit es technisch machbar ist.</p>
+<p><strong>Recht auf Auskunft, Berichtigung, Sperrung, L&ouml;schung</strong></p>
+<p>Sie haben jederzeit im Rahmen der geltenden gesetzlichen Bestimmungen das Recht auf unentgeltliche Auskunft &uuml;ber Ihre gespeicherten personenbezogenen Daten, Herkunft der Daten, deren Empf&auml;nger und den Zweck der Datenverarbeitung und ggf. ein Recht auf Berichtigung, Sperrung oder L&ouml;schung dieser Daten. Diesbez&uuml;glich und auch zu weiteren Fragen zum Thema personenbezogene Daten k&ouml;nnen Sie sich jederzeit &uuml;ber die im Impressum aufgef&uuml;hrten Kontaktm&ouml;glichkeiten an uns wenden.</p>
+<p><strong>SSL- bzw. TLS-Verschl&uuml;sselung</strong></p>
+<p>Aus Sicherheitsgr&uuml;nden und zum Schutz der &Uuml;bertragung vertraulicher Inhalte, die Sie an uns als Seitenbetreiber senden, nutzt unsere Website eine SSL-bzw. TLS-Verschl&uuml;sselung. Damit sind Daten, die Sie &uuml;ber diese Website &uuml;bermitteln, f&uuml;r Dritte nicht mitlesbar. Sie erkennen eine verschl&uuml;sselte Verbindung an der &bdquo;https://&ldquo; Adresszeile Ihres Browsers und am Schloss-Symbol in der Browserzeile.</p>
+<p><strong>Server-Log-Dateien</strong></p>
+<p>In Server-Log-Dateien erhebt und speichert der Provider der Website automatisch Informationen, die Ihr Browser automatisch an uns &uuml;bermittelt. Dies sind:</p>
+<ul>
+<li>Browsertyp und Browserversion</li>
+<li>Verwendetes Betriebssystem</li>
+<li>Referrer URL</li>
+<li>Hostname des zugreifenden Rechners</li>
+<li>Uhrzeit der Serveranfrage</li>
+<li>IP-Adresse</li>
+</ul>
+<p>Es findet keine Zusammenf&uuml;hrung dieser Daten mit anderen Datenquellen statt. Grundlage der Datenverarbeitung bildet Art. 6 Abs. 1 lit. b DSGVO, der die Verarbeitung von Daten zur Erf&uuml;llung eines Vertrags oder vorvertraglicher Ma&szlig;nahmen gestattet.</p>
+<h2>Zweck der Datenverarbeitung</h2>
+<p>Die vor&uuml;bergehende Speicherung der IP-Adresse durch das System ist notwendig, um eine Auslieferung der Website an den Rechner des Nutzers zu erm&ouml;glichen. Hierf&uuml;r muss die IP-Adresse des Nutzers f&uuml;r die Dauer der Sitzung gespeichert bleiben. In diesen Zwecken liegt auch unser berechtigtes Interesse an der Datenverarbeitung nach Art.&nbsp;6 Abs.&nbsp;1 lit.&nbsp;f DSGVO.</p>
+<h2>Dauer der Speicherung</h2>
+<p>Die Daten werden gel&ouml;scht, sobald sie f&uuml;r die Erreichung des Zweckes ihrer Erhebung nicht mehr erforderlich sind. Im Falle der Erfassung der Daten zur Bereitstellung der Website ist dies der Fall, wenn die jeweilige Sitzung beendet ist.</p>
+<p>Im Falle der Speicherung der Daten in Logfiles ist dies nach sp&auml;testens sieben Tagen der Fall. Eine dar&uuml;berhinausgehende Speicherung ist m&ouml;glich. In diesem Fall werden die IP-Adressen der Nutzer gel&ouml;scht oder verfremdet, sodass eine Zuordnung des aufrufenden Clients nicht mehr m&ouml;glich ist.</p>
+<p><strong>Kontaktformular</strong></p>
+<p>Per Kontaktformular &uuml;bermittelte Daten werden einschlie&szlig;lich Ihrer Kontaktdaten gespeichert, um Ihre Anfrage bearbeiten zu k&ouml;nnen oder um f&uuml;r Anschlussfragen bereitzustehen. Eine Weitergabe dieser Daten findet ohne Ihre Einwilligung nicht statt.</p>
+<p>Die Verarbeitung der in das Kontaktformular eingegebenen Daten erfolgt ausschlie&szlig;lich auf Grundlage Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO). Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit m&ouml;glich. F&uuml;r den Widerruf gen&uuml;gt eine formlose Mitteilung per E-Mail. Die Rechtm&auml;&szlig;igkeit der bis zum Widerruf erfolgten Datenverarbeitungsvorg&auml;nge bleibt vom Widerruf unber&uuml;hrt.</p>
+<p>&Uuml;ber das Kontaktformular &uuml;bermittelte Daten verbleiben bei uns, bis Sie uns zur L&ouml;schung auffordern, Ihre Einwilligung zur Speicherung widerrufen oder keine Notwendigkeit der Datenspeicherung mehr besteht. Zwingende gesetzliche Bestimmungen &ndash; insbesondere Aufbewahrungsfristen &ndash; bleiben unber&uuml;hrt.</p>
+<p>Im Zeitpunkt der Absendung der Nachricht werden zudem folgende Daten gespeichert:</p>
+<p>(1)&nbsp; Die IP-Adresse des Nutzers</p>
+<p>(2)&nbsp; Datum und Uhrzeit</p>
+<p>(3)&nbsp; Die von Ihnen eingegebenen und zur Verf&uuml;gung gestellten Informationen</p>
+<p>Alternativ ist eine Kontaktaufnahme &uuml;ber die bereitgestellte E-Mail-Adresse m&ouml;glich. In diesem Fall werden die mit der E-Mail &uuml;bermittelten personenbezogenen Daten des Nutzers gespeichert.</p>
+<p>Es erfolgt in diesem Zusammenhang keine Weitergabe der Daten an Dritte. Die Daten werden ausschlie&szlig;lich f&uuml;r die Verarbeitung der Konversation verwendet.</p>
+<p><strong>Newsletter-Daten</strong></p>
+<p>Zum Versenden unseres Newsletters ben&ouml;tigen wir von Ihnen eine E-Mail-Adresse. Eine Verifizierung der angegebenen E-Mail-Adresse ist notwendig und der Empfang des Newsletters ist einzuwilligen. Erg&auml;nzende Daten werden nicht erhoben oder sind freiwillig. Die Verwendung der Daten erfolgt ausschlie&szlig;lich f&uuml;r den Versand des Newsletters.</p>
+<p>Die bei der Newsletteranmeldung gemachten Daten werden ausschlie&szlig;lich auf Grundlage Ihrer Einwilligung (Art. 6 Abs. 1 lit. a DSGVO) verarbeitet. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit m&ouml;glich. F&uuml;r den Widerruf gen&uuml;gt eine formlose Mitteilung per E-Mail oder Sie melden sich &uuml;ber den &ldquo;Austragen&rdquo;-Link im Newsletter ab. Die Rechtm&auml;&szlig;igkeit der bereits erfolgten Datenverarbeitungsvorg&auml;nge bleibt vom Widerruf unber&uuml;hrt.</p>
+<p>Zur Einrichtung des Abonnements eingegebene Daten werden im Falle der Abmeldung gel&ouml;scht. Sollten diese Daten f&uuml;r andere Zwecke und an anderer Stelle an uns &uuml;bermittelt worden sein, verbleiben diese weiterhin bei uns.</p>
+<p>Quellen:</p>
+<p>Datenschutz-Konfigurator von&nbsp;<a href="http://www.mein-datenschutzbeauftragter.de/" target="_blank" rel="noopener">mein-datenschutzbeauftragter.de</a>&nbsp;&amp; Musterdatenschutzerkl&auml;rung nach EU-DSGVO der Universit&auml;t M&uuml;nster.</p>
+<p>D&uuml;sseldorf, 2018</p>
+<h1 style="color: #5e9ca0;">You can edit <span style="color: #2b2301;">this demo</span> text!</h1>
+<h2 style="color: #2e6c80;">How to use the editor:</h2>
+<p>Paste your documents in the visual editor on the left or your HTML code in the source editor in the right. <br />Edit any of the two areas and see the other changing in real time.&nbsp;</p>
+<p>Click the <span style="background-color: #2b2301; color: #fff; display: inline-block; padding: 3px 10px; font-weight: bold; border-radius: 5px;">Clean</span> button to clean your source code.</p>
+<h2 style="color: #2e6c80;">Some useful features:</h2>
+<ol style="list-style: none; font-size: 14px; line-height: 32px; font-weight: bold;">
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/01-interactive-connection.png" alt="interactive connection" width="45" /> Interactive source editor</li>
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/02-html-clean.png" alt="html cleaner" width="45" /> HTML Cleaning</li>
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/03-docs-to-html.png" alt="Word to html" width="45" /> Word to HTML conversion</li>
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/04-replace.png" alt="replace text" width="45" /> Find and Replace</li>
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/05-gibberish.png" alt="gibberish" width="45" /> Lorem-Ipsum generator</li>
+<li style="clear: both;"><img style="float: left;" src="https://html-online.com/img/6-table-div-html.png" alt="html table div" width="45" /> Table to DIV conversion</li>
+</ol>
+<p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</p>
+<h2 style="color: #2e6c80;">Cleaning options:</h2>
+<table class="editorDemoTable">
+<thead>
+<tr>
+<td>Name of the feature</td>
+<td>Example</td>
+<td>Default</td>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Remove tag attributes</td>
+<td><img style="margin: 1px 15px;" src="images/smiley.png" alt="laughing" width="40" height="16" /> (except <strong>img</strong>-<em>src</em> and <strong>a</strong>-<em>href</em>)</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove inline styles</td>
+<td><span style="color: green; font-size: 13px;">You <strong style="color: blue; text-decoration: underline;">should never</strong>&nbsp;use inline styles!</span></td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Remove classes and IDs</td>
+<td><span id="demoId">Use classes to <strong class="demoClass">style everything</strong>.</span></td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Remove all tags</td>
+<td>This leaves <strong style="color: blue;">only the plain</strong> <em>text</em>. <img style="margin: 1px;" src="images/smiley.png" alt="laughing" width="16" height="16" /></td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove successive &amp;nbsp;s</td>
+<td>Never use non-breaking spaces&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;to set margins.</td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Remove empty tags</td>
+<td>Empty tags should go!</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove tags with one &amp;nbsp;</td>
+<td>This makes&nbsp;no sense!</td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Remove span tags</td>
+<td>Span tags with <span style="color: green; font-size: 13px;">all styles</span></td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Remove images</td>
+<td>I am an image: <img src="images/smiley.png" alt="laughing" /></td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove links</td>
+<td><a href="https://html-online.com" rel="nofollow">This is</a> a link.</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove tables</td>
+<td>Takes everything out of the table.</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Replace table tags with structured divs</td>
+<td>This text is inside a table.</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Remove comments</td>
+<td>This is only visible in the source editor <!-- HELLO! --></td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Encode special characters</td>
+<td><span style="color: red; font-size: 17px;">&hearts;</span> <strong style="font-size: 20px;">☺ ★</strong> &gt;&lt;</td>
+<td><strong style="font-size: 17px; color: #2b2301;">x</strong></td>
+</tr>
+<tr>
+<td>Set new lines and text indents</td>
+<td>Organize the tags in a nice tree view.</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+<p><strong>&nbsp;</strong></p>
+<p><strong>Save this link into your bookmarks and share it with your friends. It is all FREE! </strong><br /><strong>Enjoy!</strong></p>
+<p><strong>&nbsp;</strong></p>
     </article>
 </section>
 <?php include("../footer.php"); ?>
